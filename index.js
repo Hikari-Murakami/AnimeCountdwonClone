47251a53bb65d4fadf7a40c9038ec48dc49fcd03
Hikari-Murakami <hlunhtetmin@gmail.com>
2024-01-05 15:57:37 +0630

    Views , public, index.js added (Basic Setup Done) Need To Modify

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
 
 
-console.log(process.env.ApiKey)
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+
+app.listen("8080", () => {
+  console.log("Listening In port 8080");
+});
+
+app.get("/", (req, res) => {
+  res.render("home-page.ejs");
+});
+app.get("/home", (req, res) => {
+
+  res.render("home-sub-page.ejs");
+});
